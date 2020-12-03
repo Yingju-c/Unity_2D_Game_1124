5c0c1e5bed6f0b23e4668a5406b73088442d35d3
Yingju-c <wc.rocychen@gmail.com>
2020-12-03 21:29:38 +0800

    設定牆壁與認識程式
    設定牆壁與認識程式

---
+++ Unity 2D Game/Assets/Scripts/Car.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    //單行註解
+    //number=Field
+    //Scripts:
+    //class name;
+
+    int size; //整數
+    float weight; //浮點數
+    string brand; //字串
+    bool haswindow;  //布林值
+
+}

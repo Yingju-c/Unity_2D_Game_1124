1eb90082fe01c485ebb665011a6afb8e999a6155
Yingju-c <wc.rocychen@gmail.com>
2020-12-17 21:29:17 +0800

    編寫遊戲開始程式製作下一個方塊顯示

---
+++ Unity 2D Game/Assets/Scripts/APInostatic.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class APInostatic : MonoBehaviour
+{
+    //動態屬性
+    //語法：類型 欄位名稱
+
+    //屬性
+    public Transform traA;
+    public Transform traB;
+
+    public GameObject myobj;
+
+    //方法
+    public Transform myTra;
+
+    private void Start()
+    {
+        //一般屬性的取得與設定
+    
+        //取得
+        //語法：類型欄位名稱 的 一般屬性
+        print("物件A的座標：" + traA.position);
+
+        //設定
+        //語法：類型欄位名稱 的 一般屬性 = 對應的值;
+        traB.position = new Vector3(1, 2, 3);
+
+        //練習取得與設定
+        print("我的物件圖層：" + myobj.layer);
+        myobj.layer = 2;
+
+    }
+
+    private void Update()
+    {
+        //一般方法
+        //使用
+        //語法：類型欄位名稱 的 方法(對應的參數)
+        myTra.Rotate(0, 0, 3);
+        myTra.Translate(1, 0, 0);//位移
+    }
+}
--- Unity 2D Game/Assets/Scripts/TetrisManager.cs
+++ Unity 2D Game/Assets/Scripts/TetrisManager.cs
@@ -7,30 +7,35 @@ public class TetrisManager : MonoBehaviour
 {
     #region
     //field欄位
-    [Header("fall duration"),Range(0.1f,3)]
+    [Header("fall duration"), Range(0.1f, 3)]
     public float falltime = 1.5f;//掉落時間
-    
+
     [Header("Current scores")]
     public int scores;//目前分數
-    
+
     [Header("Best scores")]
     public int bestscores;//最高分數
-    
+
     [Header("Level")]
-    public int Level =1;//等級
-    
+    public int Level = 1;//等級
+
     [Header("Go end")]
     public GameObject goend;//結束畫面
-    
+
     [Header("Sound")]
     public AudioClip brickdropsound;//方塊掉落音效
-    
+
     public AudioClip movedsound;//方塊移動與旋轉音效
-    
+
     public AudioClip removedsound;//方塊消除音效
-    
+
     public AudioClip gameoversound;//遊戲結束音效
 
+    [Header("下一個俄羅斯方塊區域")]
+    public Transform traNextArea;//Transform非靜態寫法
+
+    [Header("畫布")]
+    public Transform traCanvas;//告訴程式有一個畫布
     #endregion
 
     #region
@@ -42,10 +47,55 @@ public class TetrisManager : MonoBehaviour
     #region
     //方法
 
+    public int indexNext;
+    private void Start()
+    {
+        addbricks();
+    }
+
+    /// <summary>
+    /// 生成俄羅斯方塊
+    /// 1.隨機顯示一個下一顆俄羅斯方塊0-10
+    /// </summary>
     private void addbricks()//生成俄羅斯方塊
     {
+        //語法：下一顆編號=隨機 的 範圍(最小，最大)
+        indexNext =Random.Range(0, 11);//整數最大不包括
+        //語法：下一顆俄羅斯方塊的區域 的子物件轉成遊戲物件 的狀態 打勾
+        traNextArea.GetChild(indexNext).gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// 開始遊戲
+    /// 1.生成俄羅斯方塊
+    /// 2.上一顆隱藏
+    /// 3.隨機取下一個
+    /// </summary>
+    public void StartGame()//開始遊戲
+    {
+        //1.生成俄羅斯方塊
+        //告訴程式有一個遊戲物件
+        GameObject tetris = traNextArea.GetChild(indexNext).gameObject;
+        //並生成物件
+        //語法：Instantiate(生成命名，置入哪個父物件)
+        //告訴程式這是目前的俄羅斯方塊
+        GameObject current = Instantiate(tetris, traCanvas);
+        //API:GetComponent取得元件
+        //API:anchoredPosition座標
+        //語法：GetComponent<任何元件>()
+        //<>指<T>泛型，指所有類型
+        //語法：目前俄羅斯方塊，取得元件<介面變形>().座標=二維向量(Vx,Vy);
+        current.GetComponent<RectTransform>().anchoredPosition = new Vector2(-648, 221);
+
+        //2.上一顆隱藏
+        tetris.SetActive(false);
+
+        //3.隨機取下一個
+        addbricks();
 
     }
+
+
 
     public void addscores(int Scores)//添加分數
     {
